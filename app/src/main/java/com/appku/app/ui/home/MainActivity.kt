465537cdb4ba687a3cadd14da95be52c8dee5407
endi0712 <75595137+endi0712@users.noreply.github.com>
2020-12-28 14:53:58 +0700

    Add files via upload

--- app/src/main/java/com/appku/app/ui/home/MainActivity.kt
+++ app/src/main/java/com/appku/app/ui/home/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.appku.app.R
 import com.appku.app.databinding.ActivityMainBinding
 import com.appku.app.ui.auth.AppkuAuth
@@ -14,11 +15,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.buttonLogout.setOnClickListener {
-            AppkuAuth.logout(this){
-                startActivity(Intent(this,AuthActivity::class.java))
-                finish()
+        init()
+    }
+
+    private fun init() {
+        binding.bottonNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> navigate(R.id.homeFragment)
+                R.id.ic_news -> navigate(R.id.newsFragment)
+                R.id.ic_favorite -> navigate(R.id.favoriteFragment)
+                R.id.ic_profile -> navigate(R.id.profileFragment)
+                else -> false
             }
         }
+    }
+
+    private fun navigate(id:Int):Boolean{
+        findNavController(R.id.navHost).navigate(id)
+        return true
     }
 }
